89c7fcddc8550cc4f4120e5d899ec1372fffc633
Jérémie Gillet <jie.gillet@gmail.com>
2018-07-01 21:45:27 +0900

    Added Gaussian Elimination and cleaned up Stable Marriage

--- stableMarriage/stableMatch.hs
+++ stableMarriage/stableMatch.hs
@@ -9,20 +9,20 @@ stableMatching men women = evalState (propose (M.fromList women) men) M.empty
 propose :: (Ord a, Ord b) => Map b [a] ->
                             [(a, [b])] ->
                             State (Map b (a, [b])) [(a, b)]
-propose _ [] = get >>=  return . map (\(w, (m,_)) -> (m, w)) . M.assocs
+propose _ [] = gets $ map (\ (w, (m, _)) -> (m, w)) . M.assocs 
 propose women ((man, pref):bachelors) = do
   let theOne = head pref
   couples <- get
   case M.lookup theOne couples of
     Nothing -> do
-      modify $ M.insert theOne (man, (tail pref))
+      modify $ M.insert theOne (man, tail pref)
       propose women bachelors
     Just (boyfriend, planB) -> do
       let rank x = elemIndex x (women!theOne)
       if rank boyfriend < rank man
         then propose women $ (man, tail pref): bachelors
         else do
-          modify $ M.insert theOne (man, (tail pref)) . M.delete theOne
+          modify $ M.insert theOne (man, tail pref) . M.delete theOne
           propose women $ (boyfriend, planB): bachelors
 
 main = do
